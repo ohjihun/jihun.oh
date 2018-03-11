8d3b517b8c9501f8654b6bfba6d0356647c60697
ojh <ojhcoke@naver.com>
2018-03-11 16:59:45 +0900

    2018-03-11 17:00

---
+++ CodingTest_OhJiHun/src/prob_1/Prob_1.java
@@ -0,0 +1,37 @@
+package prob_1;
+
+public class Prob_1 {
+
+	private String  zeroChk  = ""   ;
+	private int     inputNum = 1    ;
+	private boolean zeroFlag = false;
+	
+	public Prob_1() {
+		
+	}
+
+	public String getZeroChk() {
+		return zeroChk;
+	}
+
+	public void setZeroChk(String zeroChk) {
+		this.zeroChk = zeroChk;
+	}
+
+	public int getInputNum() {
+		return inputNum;
+	}
+
+	public void setInputNum(int inputNum) {
+		this.inputNum = inputNum;
+	}
+
+	public boolean isZeroFlag() {
+		return zeroFlag;
+	}
+
+	public void setZeroFlag(boolean zeroFlag) {
+		this.zeroFlag = zeroFlag;
+	}
+	
+}
